5fb50c27093655ddc7210748415aabaa83caa4f5
Uday Varshney <uv.uday.varshney@gmail.com>
2023-03-25 02:05:20 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+  strings: ["Developer!", "Designer!"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
+var typed = new Typed(".typingg", {
+  strings: ["Developer", "Designer!"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
